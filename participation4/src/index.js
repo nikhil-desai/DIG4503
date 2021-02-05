4810d64ebd4c8fa4b4ece65d09358f2e60f70908
Nikhil Desai <nikhildesigning@gmail.com>
2021-02-05 12:15:04 -0500

    Participation Activity 4
    Making a react app to display basic local variables

---
+++ participation4/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const customer = {
+  first_name: "Alex",
+  last_name: "DeVille"
+};
+
+const output = <div>
+  <h1>My name is {customer.first_name}</h1>
+  <h1>My name is {customer.last_name}</h1>
+</div>;
+
+ReactDOM.render(output, document.querySelector("#root"));

07898d5db943077eb3b9c573f75d82808521504b
Nikhil Desai <nikhildesigning@gmail.com>
2021-02-19 12:07:57 -0500

    Adding Style
    Done w/ Lab 5

--- lab5/src/components/Posts.js
+++ lab5/src/components/Posts.js
@@ -2,25 +2,28 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Posts = () => {
+  // setting the state vars
   const [posts, setPosts] = useState();
-
+  // getting the api data w/ axios
   useEffect(() => {
     axios.get(`https://jsonplaceholder.typicode.com/posts`).then((res) => {
       const resPosts = res.data;
       setPosts(resPosts);
     });
   }, []);
+  // presenting the data w/ jsx
   return (
     <div>
+        <h1>Posts</h1>
       {posts &&
         posts.map((post) => {
           const { id, title, body } = post;
           return (
             <div key={id}>
-              <div>
-                <h1>{title}</h1>
+              <div style={{backgroundColor:'#E1E879', margin:'4px 20px'}}>
+                <h2>{title}</h2>
               </div>
-              <div>
+              <div style={{textAlign: 'left', padding:'4px 20px'}}>
                 <p>{body}</p>
               </div>
             </div>

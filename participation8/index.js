f335dd5dda6dcbaa9ad5cada54443848dad062cf
Nikhil Desai <nikhildesigning@gmail.com>
2021-03-09 09:07:07 -0500

    Finishing Participation Activity 8
    Dynamic Routing

---
+++ participation8/index.js
@@ -0,0 +1,21 @@
+import Express from "Express";
+// Express Definitions
+const App = Express();
+const port = 3000;
+
+// Default Route
+App.get("/", function (request, response) {
+  response.send("App is running.");
+});
+// Favorite Candy Route
+App.get("/favorite/:candy", function (request, response) {
+  response.send("Your favorite candy is: " + request.params.candy);
+});
+// Names Route
+App.get("/leastfavorite/:candy", function (request, response) {
+  response.send("Your least favorite candy is: " + request.params.candy);
+});
+//Running the App
+App.listen(port, () => {
+  console.log("Running server...");
+});

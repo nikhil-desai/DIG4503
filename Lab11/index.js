f5d85e455fa4dd11dbe85fb31167cf8ca30a45a1
Nikhil Desai <61434199+nikhil-desai@users.noreply.github.com>
2021-04-24 20:23:57 -0400

    Adding Portfolio 2 Changes
    Chalk and Boxen Module

--- Lab11/index.js
+++ Lab11/index.js
@@ -1,4 +1,9 @@
 import MongoClient from "mongodb";
+// Adding Chalk Module ES6 Version
+import chalk from 'chalk';
+// Adding boxen
+import boxen from 'boxen';
+
 // Database URL
 const URL = "mongodb+srv://ndesai:FjZPHv5M0Jie7Qxb@cluster0.oewwc.mongodb.net/";
 
@@ -16,7 +21,10 @@ MongoClient.connect(URL, { useUnifiedTopology: true })
     // Listing Airbnb data
     cursor.forEach(
       (document) => {
-        console.log(document.name);
+        // Blue Bold Listing & Blue Background || Green Bold Price & Green BG
+        // Every item is boxed using 'boxen'
+        console.log(boxen(chalk.blue.bold('Listing Name: ') + chalk.black.bgBlue(document.name) + 
+        '\n' + chalk.green.bold('Price: ') + chalk.black.bgGreen(document.price), {padding: 1}));
       },
       () => {
         connection.close();
